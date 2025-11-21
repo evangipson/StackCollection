2c7562200a4c8003ccdccca2aca1ad3f2c3462c5
Evan Gipson <evangipson@gmail.com>
2025-11-21 10:49:01 -0600

    Update docs for extension methods

--- src/StackCollection/StackCollectionExtensions.cs
+++ src/StackCollection/StackCollectionExtensions.cs
@@ -3,8 +3,7 @@
 namespace StackCollection;
 
 /// <summary>
-/// A <see langword="static"/> collection of methods to extend the
-/// functionality of a stack collection.
+/// A <see langword="static"/> collection of methods to extend the functionality of a stack collection.
 /// </summary>
 public static class StackCollection
 {
@@ -17,7 +16,7 @@ public static class StackCollection
         where T : allows ref struct;
 
     /// <summary>
-    /// Creates a dynamically-sized buffer for a <see cref="StackCollection{T}"/>.
+    /// Creates a dynamically-sized <see cref="StackCollection{T}"/>.
     /// </summary>
     /// <typeparam name="T">The type of stack collection element.</typeparam>
     /// <param name="capacity">The maximum number of elements.</param>
